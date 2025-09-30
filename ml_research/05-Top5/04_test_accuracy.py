5ba7d7cee828475c902521da5efe036ca3dcb8ab
Pavel Yakushenko <75137738+steelfeet@users.noreply.github.com>
2025-09-30 07:28:59 +0300

    Add files via upload

---
+++ ml_research/05-Top5/04_test_accuracy.py
@@ -0,0 +1,89 @@
+
+import os, random
+import csv, json, shutil
+import glob
+import requests
+
+from tqdm import tqdm
+
+
+from sentence_transformers import SentenceTransformer
+from scipy import spatial
+from PIL import Image
+import pandas as pd
+import numpy as np
+import copy
+import os
+
+
+from sentence_transformers import SentenceTransformer
+from PIL import Image
+
+model_name = 'clip-ViT-B-16'
+st_model = SentenceTransformer(model_name)
+
+CLASSES_DIR = os.path.join("d:\\", "_vault", "02-ClearDS", "data")
+RESULT_DIR = os.path.join("d:\\", "_vault", "04-Cosin")
+
+
+# функция сортировки 
+def order_of_item(item):
+    return float(item["cos_dist"])
+
+
+def calculate_cos_dist(emb_a: np.array, emb_b: np.array) -> float:
+    result_distance = spatial.distance.cosine(emb_a, emb_b)
+    return result_distance
+
+def found_similar_images(input_img_path: str, images_db, n: int=2):
+    similar_classes = []
+    emb_b = vectorize_img(input_img_path)
+
+    for result_item in images_db:
+        emb_a = np.array(result_item["emb"])
+        cos_dist = calculate_cos_dist(emb_a, emb_b)
+        similar_item = {}
+        similar_item["class_n"] = result_item["class_n"]
+        similar_item["cos_dist"] = cos_dist
+        similar_item["filename"] = result_item["filename"]
+
+        similar_classes.append(similar_item)
+
+    # сортируем
+    sorted_list = sorted(similar_classes, key=order_of_item)
+
+    return sorted_list[:n]
+
+
+def vectorize_img(img_path, model=st_model):
+    img = Image.open(img_path)
+    return model.encode(img)
+
+
+CLASSES_DIR = os.path.join("d:\\", "_vault", "02-ClearDS", "data")
+RESULT_DIR = os.path.join("d:\\", "_vault", "04-Cosin")
+
+
+# загружаем нашу базу эмббедингов
+result_path = os.path.join(RESULT_DIR, "result.json")
+with open(result_path, 'r') as file:
+    images_db = json.load(file)
+
+
+# очищаем config.UPLOAD_DIR
+images_n = 0
+accuracy_n = 0
+for class_n in tqdm(os.listdir(CLASSES_DIR)):
+    class_dir = os.path.join(CLASSES_DIR, class_n)
+    for filename in os.listdir(class_dir):
+        current_file_path = os.path.join(class_dir, filename)
+        sorted_list = found_similar_images(current_file_path, images_db, 5)
+
+        images_n += 1
+        for similar_class in sorted_list:
+            if (int(class_n) == int(similar_class["class_n"])) and (similar_class["filename"] != filename):
+                accuracy_n += 1
+                break
+
+formatted_accuracy = accuracy_n/images_n * 100
+print(f"images_n: {images_n}; accuracy_n: {accuracy_n}, accuracy: {formatted_accuracy:.6f}%")
---
+++ ml_research/05-Top5/mnet.predict_top5.py
@@ -0,0 +1,129 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader, random_split
+
+import os, sys
+import json, random
+import pathlib, cv2
+from tqdm import tqdm
+
+from sqlalchemy import and_
+
+
+from PIL import Image, ImageDraw
+
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.path.join("d:\\", "_vault", "02-ClearDS", "data")
+MODEL_DIR = os.path.join("d:\\", "_vault", "02-ClearDS", "models")
+
+IM_SIZE = 512
+
+# сбрасывать ли последний слой (при смене и объединении классов)
+RESET_LINEAR = False
+
+# текущая (промежуточного сохранения)
+MODEL_NAME = "mnet_3"
+
+TOP_CLASSES = 5
+
+IM_SIZE = 512
+# текущая (промежуточного сохранения)
+# лучшая
+BEST_MODEL_NAME = f"{MODEL_NAME}_best"
+
+
+
+full_predicted = 0
+full_n = 0
+full_stat_dict = {}
+
+
+ValidTransform = A.Compose([
+    A.Resize(IM_SIZE,IM_SIZE),
+    A.CenterCrop(IM_SIZE,IM_SIZE),
+    A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ToTensorV2()])
+
+if __name__ == '__main__':
+
+    ds_dir = DATA_DIR
+    subfolders_n = [ int(os.path.basename(f)) for f in os.scandir(ds_dir) if f.is_dir() ]
+    NUM_CL = max(subfolders_n) + 1
+    print(f"NUM_CL = {NUM_CL}")
+
+    model = torchvision.models.mobilenet_v3_large(weights=torchvision.models.MobileNet_V3_Large_Weights.IMAGENET1K_V1)
+
+    #перенастраиваем модель под наши классы
+    for param in model.parameters():
+        param.requires_grad = False
+    n_inputs = model.classifier[0].in_features
+    last_layer = nn.Linear(n_inputs, NUM_CL)
+    model.classifier = last_layer
+
+    # если есть - загружаем
+    BEST_MODEL_PATH = pathlib.Path(MODEL_DIR, f"{BEST_MODEL_NAME}.pth")
+    print(BEST_MODEL_PATH)
+    if BEST_MODEL_PATH.is_file():
+        print("best file exist, load")
+        model.load_state_dict(torch.load(BEST_MODEL_PATH))
+    else:
+        print("no best model")
+    model.eval()
+
+
+
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
+    torch.cuda.empty_cache()
+    print("device: ", device)
+    print()
+
+    for class_id in tqdm(os.listdir(DATA_DIR)):
+        class_dir = os.path.join(DATA_DIR, class_id)
+        for filename in os.listdir(class_dir):
+            current_file_path = os.path.join(class_dir, filename)
+
+            im_new = Image.open(current_file_path)
+            image_array = np.array(im_new.getdata()).reshape(im_new.size[0], im_new.size[1], 3).astype(np.float32)
+            image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+            
+            
+            input_img = ValidTransform(image=image_array)
+            # unsqueeze batch dimension, in case you are dealing with a single image
+            input_img = input_img["image"].unsqueeze(0)
+            input_img = input_img.to(device)
+            # Get prediction
+            with torch.no_grad():
+                logits = model(input_img)
+            """
+            ps = torch.exp(logits)        
+            _, top_class = ps.topk(1, dim=1)
+            """
+            top_classes = []
+            top_classes = np.argsort(logits.cpu(), axis=1)[0].tolist()
+            top_classes.reverse()
+            """
+            print()
+            print(f"{class_id}")
+            print(top_classes[:TOP_CLASSES])
+            """
+
+            data_item = {}
+            if int(class_id) in top_classes[:TOP_CLASSES]: 
+                full_predicted += 1
+
+            full_n += 1
+
+
+
+print(f"top {TOP_CLASSES} session accuracy: {full_predicted} / {full_n} = {(full_predicted / full_n):.6f}")
+print("success")
